8d3447488efbad89e10af5f834f61bd6728697e9
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-02 03:14:46 +0700

    add configureMapper

---
+++ AppCamera/AppCamera.Web/App_Start/ConfigureMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace AppCamera.Web.App_Start
+{
+    public static class ConfigureMapper
+    {
+        public static void Register()
+        {
+            Mapper.Initialize(map =>
+            {
+               /*
+                * ...........
+                */
+            });
+        }
+    }
+}
--- AppCamera/AppCamera.Web/Global.asax.cs
+++ AppCamera/AppCamera.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppCamera.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,7 @@ namespace AppCamera.Web
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ConfigureMapper.Register();
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
